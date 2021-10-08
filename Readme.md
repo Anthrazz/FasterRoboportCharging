b2d4f78422318b255f69276169b93ac65ac0991e
Anthrazz <25553648+Anthrazz@users.noreply.github.com>
2021-10-08 20:32:16 +0200

    initial release

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Faster Roboport Charging
+
+A Factorio mod to make the charging speed and the service radius of roboports configurable via the mod settings.
+
+All variables can be changed via the ingame mod settings (needs game relaunch).
+
+## Default Changes to Base Game
+
+* Charging Speed per Robot `1MW` -> `4MW`
+* Charging Speed of Roboports internal Battery `5MW` -> `40MW`
+* Logistic Radius stays at default `25` Radius (50x50 Tiles)
+* Construction Radius stays at default `55` Radius (110x110 Tiles)
---
+++ settings.lua
@@ -0,0 +1,26 @@
+data:extend({
+	{
+		type = "string-setting",
+		name = "FasterRoboportCharging-robo-charging-speed",
+		setting_type = "startup",
+		default_value = "10MW",
+	},
+    {
+		type = "string-setting",
+		name = "FasterRoboportCharging-roboport-charging-speed",
+		setting_type = "startup",
+		default_value = "40MW",
+	},
+	{
+		type = "int-setting",
+		name = "FasterRoboportCharging-roboport-logistics-radius",
+		setting_type = "startup",
+		default_value = "25",
+	},
+	{
+		type = "int-setting",
+		name = "FasterRoboportCharging-roboport-construction-radius",
+		setting_type = "startup",
+		default_value = "55",
+	},
+})
